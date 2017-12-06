ef13535035ad87f7b634bbb026824460c7e54904
danielcostta <danieljunnio@hotmail.com>
2017-12-06 20:24:27 -0200

    Atributos definidos, classe produto e interface operacao criadas

---
+++ TesteCedro/Models/Operacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteCedro.Models
+{
+    interface Operacao
+    {
+        List<Produto> PegaProdutos();
+
+        void AdicionaProduto(Produto produto);
+
+        void EditaProduto(Produto produto);
+
+        void ExcluiProduto(int id);
+
+        void DetalhaProduto(int id);
+
+        void CompraProduto(int id);
+
+    }
+}
---
+++ TesteCedro/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteCedro.Models
+{
+    public class Produto
+    {
+        [Required]
+        public int IDProduto { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public float Valor { get; set; }
+
+        [Required]
+        public string Imagem { get; set; }
+
+    }
+}
