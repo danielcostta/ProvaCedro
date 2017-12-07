cbf8cd4ce70259ec50f402d381af456efb3ee10f
danielcostta <danieljunnio@hotmail.com>
2017-12-06 23:06:36 -0200

    Controlador configurado

--- TesteCedro/Controllers/HomeController.cs
+++ TesteCedro/Controllers/HomeController.cs
@@ -10,23 +10,92 @@ namespace TesteCedro.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
+            Rota rota = new Rota();
+            List<Produto> ListaProdutos = rota.PegaProdutos().ToList();
+            return View("Lista", ListaProdutos);
+        }
+
+        [HttpGet]
+        public IActionResult AdicionaProduto()
+        {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public IActionResult AdicionaProduto(Produto produto)
         {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                Rota rota = new Rota();
+                rota.AdicionaProduto(produto);
+                return RedirectToAction("Index");
+            }
         }
 
-        public IActionResult Contact()
+        [HttpGet]
+        public IActionResult EditaProduto(int id)
         {
-            ViewData["Message"] = "Your contact page.";
+            Rota rota = new Rota();
+            Produto produto = rota.PegaProdutos().Single(item => item.IDProduto == id);
+            return View(produto);
+        }
 
-            return View();
+        [HttpPost]
+        public IActionResult EditaProduto(Produto produto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(produto);
+            }
+
+            Rota rota = new Rota();
+            rota.EditaProduto(produto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult ExcluiProduto(int id)
+        {
+            Rota rota = new Rota();
+            Produto produto = rota.PegaProdutos().Single(item => item.IDProduto == id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult ExcluiProduto(Produto produto)
+        {
+            Rota rota = new Rota();
+            rota.ExcluiProduto(produto.IDProduto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult DetalhaProduto(int id)
+        {
+            Rota rota = new Rota();
+            Produto produto = rota.PegaProdutos().Single(item => item.IDProduto == id);
+            return View(produto);
+        }
+
+        [HttpGet]
+        public IActionResult CompraProduto(int id)
+        {
+            Rota rota = new Rota();
+            Produto produto = rota.PegaProdutos().Single(item => item.IDProduto == id);
+            return View(produto);
+        }
+
+        public IActionResult Lista()
+        {
+            return View("Index");
         }
 
         public IActionResult Error()
